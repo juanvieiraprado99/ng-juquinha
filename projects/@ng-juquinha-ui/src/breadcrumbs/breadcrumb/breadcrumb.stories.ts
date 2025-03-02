5e721569d46b181403f42fe2ec88241a043fe68d
juanvieiraprado99 <juanprado99@gmail.com>
2025-03-02 15:13:16 -0300

    fix: ajust project name

---
+++ projects/@ng-juquinha-ui/src/breadcrumbs/breadcrumb/breadcrumb.stories.ts
@@ -0,0 +1,67 @@
+import type { Meta, StoryObj } from "@storybook/angular"
+import { BreadcrumbComponent } from "./breadcrumb.component"
+
+const meta: Meta<BreadcrumbComponent> = {
+  title: "Breadcrumbs/Breadcrumb",
+  component: BreadcrumbComponent,
+  tags: ["autodocs"],
+  argTypes: {
+    breadcrumbItems: {
+      description:
+        'List of items in breadcrumb.<br/>Use BreadcrumbItem model imported from "@ng-juquinha-ui".',
+    },
+    lastIsCurrent: {
+      control: "boolean",
+      description: "Apply activated style on last item of breadcrumb.",
+      type: {
+        name: "boolean",
+        required: false,
+      },
+    },
+  },
+}
+
+export default meta
+type Story = StoryObj<BreadcrumbComponent>
+
+export const defaultBreadcrumb: Story = {
+  args: {
+    breadcrumbItems: [
+      {
+        name: "Página inicial",
+        icon: "home",
+        link: "/",
+      },
+      {
+        name: "Listagem de usuários",
+        link: "/usuarios",
+        icon: "users",
+      },
+      {
+        name: "Usuário Nº 1",
+      },
+    ],
+    lastIsCurrent: true,
+  },
+}
+
+export const breadcrumbLastDisabled: Story = {
+  args: {
+    breadcrumbItems: [
+      {
+        name: "Página inicial",
+        icon: "home",
+        link: "/",
+      },
+      {
+        name: "Listagem de usuários",
+        link: "/usuarios",
+        icon: "users",
+      },
+      {
+        name: "Usuário Nº 1",
+      },
+    ],
+    lastIsCurrent: false,
+  },
+}
